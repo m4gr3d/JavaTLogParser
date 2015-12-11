858311a953f8db815743e8021e63aeed74492db9
Chavi Weingarten <chavi@3drobotics.com>
2015-12-10 20:32:30 -0800

    Added support for mutliple files. Added jar file

--- src/com/o3dr/tlog/Main.java
+++ src/com/o3dr/tlog/Main.java
@@ -6,53 +6,68 @@ import java.util.Date;
 import java.util.List;
 
 import com.MAVLink.Messages.MAVLinkMessage;
-import com.MAVLink.common.msg_heartbeat;
-import com.MAVLink.common.msg_named_value_int;
 
 public class Main {
+    private static String[] inputArgs;
+    private static Date date = new Date();
 
     public static void main(String[] args) {
-        final Date date = new Date();
-        for (String file : args) {
-            System.out.println("file: " + file);
-            try {
-                URI uri = new URI(file);
-                TLogParser.getAllEventsAsync(uri, new TLogParser.TLogParserFilter() {
+        inputArgs = args;
+        parseTLog(0);
+    }
 
-                    @Override
-                    public boolean addEventToList(TLogParser.Event event) {
-                        // add your own filter
-                        // return 180 == event.getMavLinkMessage().msgid;
+    private static void parseTLog(final int fileInterval) {
+        if (inputArgs.length <= fileInterval) {
+            return;
+        }
 
-                        return true;
+        final String file = inputArgs[fileInterval];
+
+        System.out.println("file: " + file);
+        try {
+            URI uri = new URI(file);
+            TLogParser.getAllEventsAsync(uri, new TLogParser.TLogParserFilter() {
+
+                @Override
+                public boolean addEventToList(TLogParser.Event event) {
+                    // add your own filter
+                    // return 180 == event.getMavLinkMessage().msgid;
+
+                    return true;
+                }
+
+                @Override
+                public boolean continueIterating() {
+                    return true;
+                }
+
+            }, new TLogParser.TLogParserCallback() {
+                @Override
+                public void onResult(List<TLogParser.Event> eventList) {
+                    System.out.println("Success " + (eventList == null ? 0 : eventList.size()));
+                    for (TLogParser.Event event : eventList) {
+                        // msg_heartbeat heartbeat = (msg_heartbeat)event.getMavLinkMessage();
+                        long timestamp = event.getTimestamp();
+                        MAVLinkMessage mavlinkMsg = event.getMavLinkMessage();
+                        date.setTime(timestamp);
+                        System.out.println("event time " + date.toLocaleString() + " id:" + mavlinkMsg.msgid + " " + timestamp + " " + mavlinkMsg);
                     }
 
-                    @Override
-                    public boolean continueIterating() {
-                        return true;
-                    }
+                    System.out.println();
+                    parseTLog(fileInterval+1);
+                }
 
-                }, new TLogParser.TLogParserCallback() {
-                    @Override
-                    public void onResult(List<TLogParser.Event> eventList) {
-                        System.out.println("Success " + (eventList == null ? 0 : eventList.size()));
-                        for (TLogParser.Event event : eventList) {
-                            // msg_heartbeat heartbeat = (msg_heartbeat)event.getMavLinkMessage();
-                            long timestamp = event.getTimestamp();
-                            MAVLinkMessage mavlinkMsg = event.getMavLinkMessage();
-                            date.setTime(timestamp);
-                            System.out.println("event time " + date.toLocaleString() + " id:" + mavlinkMsg.msgid + " " + timestamp + " " + mavlinkMsg);
-                        }
-                    }
-
-                    @Override
-                    public void onFailed(Exception e) {
-                        System.out.println("Failed to get message " + e);
-                    }
-                });
-            } catch (URISyntaxException e) {
-                System.out.println("Failed to get uri from tlog path");
-            }
+                @Override
+                public void onFailed(Exception e) {
+                    System.out.println("Failed to get message " + e);
+                    System.out.println();
+                    parseTLog(fileInterval+1);
+                }
+            });
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to get uri from tlog path");
+            System.out.println();
+            parseTLog(fileInterval+1);
         }
     }
 }
